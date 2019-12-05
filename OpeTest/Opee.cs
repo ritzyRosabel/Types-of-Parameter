5b028c8f6e0cd3b9a129899ef348ccf5b64da8f4
Emmanuel Ebelebe <ebelebeemmanuel@yahoo.com>
2019-12-05 18:03:11 +0100

    first commit

---
+++ OpeTest/Opee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OpeTest
+{
+    public  class Opee
+    {
+        //name parameter
+        public static string Call(string s1, string s2,string s3)
+        {
+            return s1 + s2 + s3;
+        }
+
+        public static int calling(int s1, int s2)
+        {
+            return s1 + s2;
+        }
+        //ref parameter
+        public static void val( ref string val1)
+        {
+            if (val1 == "dog")
+            {
+              Console.WriteLine("matched");
+            }
+
+            val1 = "cat";
+        }
+        public static void cube(ref int x)
+        {
+            x = x * x * x;
+            Console.WriteLine(x);
+        }
+
+        public static void mult(ref int x, ref int y)
+        {
+            x = x * y;
+        }
+        //out parameter
+        public static int square(int a, out int b)
+        {
+            b = a * a;
+            return b;
+        }
+    }
+}
---
+++ OpeTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OpeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //named parameter
+            Console.WriteLine(Opee.Call(s2: "Geek", s3: "call", s1: "me")); 
+            Console.WriteLine(Opee.calling(s1: 2, s2: 34));
+
+            // ref parameter
+            string vall = "dog";
+            Opee.val(ref vall);
+            Console.WriteLine(vall);
+           int  y = 5;
+           Opee.cube(ref y);
+            int x = 10;
+           Console.WriteLine(x+" "+ y);
+           Opee.mult(ref x, ref y);
+           Console.WriteLine(x + " " + y);
+
+            // out parameter
+            int a, b;
+            a =  Opee.square(10, out b);
+            Console.WriteLine("the area of the square with side 10 cm is :" + a);
+            Console.ReadLine();
+
+        }
+    }
+}
